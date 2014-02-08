4042dde54fc9f046b0b9186d89b98bb0c80bec6b
John Schindler <John.Schindler@utexas.edu>
2014-02-07 20:41:00 -0600

    adding Euler problem #10

---
+++ Euler_10.py
@@ -0,0 +1,31 @@
+
+"""method to find sum of all primes below @param limit"""
+def sum_primes_below_limit(limit):
+	sum = 0
+	primes = [False] * (limit + 1)
+	index = 2
+	multiplier = 2
+
+	while index <= limit:
+		multiplier = 2
+		while multiplier * index <= limit:
+			primes[multiplier * index] = True
+			multiplier += 1
+		index += 1
+
+	index = 2
+	while index <= limit:
+		
+		if primes[index] == False:
+			sum += index
+		index += 1
+
+	return sum
+
+
+print sum_primes_below_limit(2000000)
+
+
+
+
+
