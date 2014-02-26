5f764cab3afd0053379146d56c734c77dd698c92
John Schindler <john02@cs.utexas.edu>
2014-02-25 18:56:05 -0600

    little linked list class finished for now

--- test_practice_1.py
+++ test_practice_1.py
@@ -1,8 +1,8 @@
-def Class Double_Linked_List(object)	:
+class Double_Linked_List(object)	:
 
 
-	def Class Node()	:
-		node_class_var = "The Node Class Var"
+	class Node(object)	:
+		node_class_var = 99
 
 
 		def __init__(self, data, prev, next)	:
@@ -17,21 +17,52 @@ def Class Double_Linked_List(object)	:
 		self.tail = None
 
 
-	def add_first(data) :
+	def add_first(self, data) :
 		if(self.size == 0):
-			temp_node = Node(data, None, None)
+			temp_node = Double_Linked_List.Node(data, None, None)
+			self.head = temp_node
+			self.tail = temp_node
 		else:
 			temp_node = Node(data, None, self.head)
+			temp_node = self.head
 
 		self.size += 1	
 
 
-
-	def add(data)	:
-		#if size == 0 :
+	def add(self, data)	:
 		if self.size == 0 :
-			add_first(data)
+			self.add_first(data)
 		else:
+			temp_node = Double_Linked_List.Node(data, None, None)
+			self.tail.next = temp_node
+			self.tail = temp_node
+
+		self.size += 1
+
+
+	def get(self, pos) : 
+		if self.size == 0 or pos > self.size:	
+			return None
+
+		this_pos = 0
+		temp_node = self.head
+
+		while this_pos < pos :
+			temp_node = temp_node.next			
+
+		return temp_node.data
+
+
+
+my_list = Double_Linked_List()
+
+my_list.add(10)
+my_list.add(20)
+
+print(my_list.get(0))
+
+
+
 			
 
 
